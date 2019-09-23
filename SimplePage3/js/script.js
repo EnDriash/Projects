077dbb1e7e0830d8f4237bd66de0d569f081d7be
Jędrzej Krawczyk <siewierjedr@gmail.com>
2019-09-23 18:31:35 +0200

    Applied better structure of project in SimplePage2/ Added mobile and desktop navigation for SimplePage 3

---
+++ SimplePage3/js/script.js
@@ -0,0 +1,26 @@
+const burger = document.querySelector(".hamburger");
+const nav = document.querySelector("nav");
+const sectionList = document.querySelectorAll("nav li");
+
+burger.addEventListener('click', () =>{
+    nav.classList.contains("show") ? nav.classList.remove("show") : nav.classList.add("show");
+});
+
+const sectionBlocks = [
+    ".jumbotron",
+    ".services", 
+    ".portfolio", 
+    ".aboutus", 
+    ".contact"
+];
+
+sectionList.forEach((element, index) => {
+    element.addEventListener('click', function(e) {
+        let x = document.querySelector(sectionBlocks[index])
+        x.scrollIntoView();
+        nav.classList.remove("show");
+        e.preventDefault();
+        
+    });
+});
+
